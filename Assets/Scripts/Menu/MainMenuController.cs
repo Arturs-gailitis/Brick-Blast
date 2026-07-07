ad25c20c320e1e5ca14fd958f49e7eacb46a8f24
Arturs-gailitis <artursgailitis2003@gmail.com>
2026-07-07 09:37:28 +0300

    Swirching scenes have a sound effect

--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -1,12 +1,64 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    [Header("Scene")]
     [SerializeField] private string SceneName;
+
+    [Header("Sound")]
+    [SerializeField] private UIButtonClickSound buttonClickSound;
+    [SerializeField] [Min(0f)] private float sceneLoadDelay;
+
+    private Button button;
+    private bool isLoading;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+
+        if (buttonClickSound == null)
+        {
+            buttonClickSound = GetComponent<UIButtonClickSound>();
+        }
+    }
+
+    private void Start()
+    {
+        if (buttonClickSound != null)
+        {
+            buttonClickSound.enabled = false;
+        }
+    }
 
     public void SwitchGame()
     {
+        if (isLoading || string.IsNullOrWhiteSpace(SceneName))
+        {
+            return;
+        }
+
+        StartCoroutine(LoadSceneAfterSound());
+    }
+
+    private IEnumerator LoadSceneAfterSound()
+    {
+        isLoading = true;
+
+        if (buttonClickSound != null)
+        {
+            buttonClickSound.OnPointerClick(null);
+        }
+
+        if (button != null)
+        {
+            button.interactable = false;
+        }
+
+        yield return new WaitForSecondsRealtime(sceneLoadDelay);
+
         SceneManager.LoadScene(SceneName);
     }
 }
